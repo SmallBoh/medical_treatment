8d36f21f1715e9ed170c083db55b9d0902ea8b22
刘高坡 <smallboh123@sina.com>
2014-11-25 09:22:29 +0800

    首次提交，框架。

---
+++ test/src/net/yasite/api/params/ArtInfoParams.java
@@ -0,0 +1,13 @@
+package net.yasite.api.params;
+
+public class ArtInfoParams extends BaseHttpParam{
+	private int page;
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+}
---
+++ test/src/net/yasite/api/params/DCInfoParams.java
@@ -0,0 +1,14 @@
+package net.yasite.api.params;
+
+
+public class DCInfoParams extends BaseHttpParam{
+	private int page;
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+}
